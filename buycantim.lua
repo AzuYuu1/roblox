67576071868ad64d51fa831381ed9436b8026290
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-07 17:51:13 +0700

    Create buycantim.lua

---
+++ buycantim.lua
@@ -0,0 +1,48 @@
+local saveModule = require(game:GetService("ReplicatedStorage").Library.Client.Save).Get()
+local Network = game.ReplicatedStorage:WaitForChild('Network')
+local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
+
+local rodNames = {
+
+    AdvancedFishing = {
+
+        ["Amethyst Fishing Rod"] = {2250000},
+
+    },
+}
+
+function getActive() return workspace.__THINGS.__INSTANCE_CONTAINER.Active:GetChildren()[1] end
+
+function checkForRod(rodName)
+    for uid,value in pairs(saveModule.Inventory.Misc)  do
+        for _,v in pairs(value) do
+            if v == rodName  then
+                return true 
+            end
+        end
+    end
+    return false
+end
+
+function buyRod(rodName) return Network:WaitForChild("FishingMerchant_PurchaseRod"):InvokeServer(rodName) end
+
+local Area = tostring(getActive())
+local areaRodNames = rodNames[Area]
+local desiredRod = ""
+if Area == "Fishing" and not checkForRod("Wooden Fishing Rod") then
+    Network.Instancing_FireCustomFromClient:FireServer("Fishing", "ClaimRod") 
+    desiredRod = "Pro Fishing Rod"
+elseif Area == "AdvancedFishing" and not checkForRod("Amethyst Fishing Rod") then
+    desiredRod = "Amethyst Fishing Rod"
+end
+
+while true do task.wait(10)
+    if areaRodNames and not checkForRod(desiredRod) then
+        for rodName, price in pairs(areaRodNames) do
+            if not checkForRod(rodName) and Library.CurrencyCmds.Get("Fishing") > price[1] then
+                buyRod(rodName)
+                warn("bought "..rodName, price[1])
+            end
+        end
+    end
+end
