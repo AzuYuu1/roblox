5a9c5a73ca7d7005162d4e8020e3ef64e1179e3e
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-09 03:59:16 +0700

    Create autouseitem.lua

---
+++ autouseitem.lua
@@ -0,0 +1,34 @@
+task.wait(1)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Get"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
+
+
+
+--auto use fruits, postion
+task.wait(3)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("69c7fbeed3524224aaabeb54e3b2e1ee",1)
+task.wait(1)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("9fb26a7a409f4965b18d88b3d78651d2",1)
+task.wait(1)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("b854636485bc46c69e07108ed2c99194",)
+task.wait(1)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("6d8605ac7bda42508ba13fcf9893dbf7")
+
+task.wait(5)
+--mang sach coin 7
+task.wait(0.3)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
+task.wait(0.3)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
+task.wait(0.3)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
+task.wait(0.3)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
+task.wait(0.3)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
+
