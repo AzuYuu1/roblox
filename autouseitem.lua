ca82c65d13fa77a670b915da1e93dce2afa663af
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-10 05:24:24 +0700

    Update autouseitem.lua

--- autouseitem.lua
+++ autouseitem.lua
@@ -56,39 +56,34 @@ Workspace.DescendantAdded:Connect(function(v)
 end)
 
 _G.loop = true
-_G.loopDelay = 15
-while _G.loop do
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
-task.wait(loopDelay)
-end
-
-
---auto use fruits, postion
-_G.loop = true
 _G.loopDelay = 5
 while _G.loop do
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBall_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("ToyBone_Consume"):InvokeServer()
+task.wait(0.5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("SqueakyToy_Consume"):InvokeServer()
+
+--auto use fruits, postion
+
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("69c7fbeed3524224aaabeb54e3b2e1ee",2)
 task.wait(0.5)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("9fb26a7a409f4965b18d88b3d78651d2",2)
@@ -107,13 +102,10 @@ task.wait(0.5)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("49244dda524848b19d42438af7b337ff",10)
 task.wait(0.5)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("49244dda524848b19d42438af7b337ff",10)
-task.wait(loopDelay)
-end
+
 task.wait(3.5)
 --fluxus
-_G.loop = true
-_G.loopDelay = 5
-while _G.loop do
+
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("6d8605ac7bda42508ba13fcf9893dbf7")
 task.wait(0.5)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("36b2906ccac548a48536f3fdca803bdc")
@@ -134,14 +126,9 @@ task.wait(0.5)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("d35c953fa68a441da7b40f560a00bedb",5)
 task.wait(0.5)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("993d741352e8418d8dd58b1da6d3198c",5)
-task.wait(loopDelay)
-end
 
 
-task.wait(5)
-_G.loop = true
-_G.loopDelay = 5
-while _G.loop do
+
 --mang sach coin 7
 task.wait(0.3)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
@@ -159,13 +146,9 @@ task.wait(0.3)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("ca23a1a46f0c4c00beb92fc949576594")
 task.wait(0.3)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("ca23a1a46f0c4c00beb92fc949576594")
-task.wait(loopDelay)
-end
+
 --use coin flag for codex
 task.wait(5)
-_G.loop = true
-_G.loopDelay = 300
-while _G.loop do
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Flags: Consume"):InvokeServer("Coins Flag","a1b47d1d9f28407db7d5b19a1d71d438")
     task.wait(loopDelay)
 end
