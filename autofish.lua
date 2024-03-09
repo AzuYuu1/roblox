5d1599e41fc4cd8d5870b2cf40350325889d3cba
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-10 04:59:02 +0700

    Update autofish.lua

--- autofish.lua
+++ autofish.lua
@@ -1,3 +1,4 @@
+--remove texture
 repeat
     task.wait()
 until game:IsLoaded()
@@ -60,8 +61,77 @@ Workspace.DescendantAdded:Connect(function(v)
 end)
 
 --teleport
-repeat task.wait() until game:IsLoaded() task.wait(2.5)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.workspace.__THINGS.Instances.Fishing.Teleports.Enter.CFrame.Position)
-task.wait(15)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer("Fishing", "ClaimRod",Vector3.new(1139, 75, -3445))
 
+
+_G.loop = true
+_G.loopDelay = 0.00001
+while _G.loop do
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer("AdvancedFishing","RequestCast",Vector3.new(1452.843505859375, 61.114078521728516, -4437.3466796875))
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer("AdvancedFishing","RequestReel")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_InvokeCustomFromClient"):InvokeServer("AdvancedFishing","Clicked")
+task.wait(loopDelay)
+end
+
+--buy rod
+_G.loop = true
+_G.loopDelay = 20
+while _G.loop do
+local saveModule = require(game:GetService("ReplicatedStorage").Library.Client.Save).Get()
+local Network = game.ReplicatedStorage:WaitForChild('Network')
+local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
+
+local rodNames = {
+
+    AdvancedFishing = {
+        ["Amethyst Fishing Rod"] = {2250000},
+    },
+}
+
+function getActive() return workspace.__THINGS.__INSTANCE_CONTAINER.Active:GetChildren()[1] end
+
+function checkForRod(rodName)
+    for uid,value in pairs(saveModule.Inventory.Misc)  do
+        for _,v in pairs(value) do
+            if v == rodName  then
+                return true 
+            end
+        end
+    end
+    return false
+end
+
+function buyRod(rodName) return Network:WaitForChild("FishingMerchant_PurchaseRod"):InvokeServer(rodName) end
+
+local Area = tostring(getActive())
+local areaRodNames = rodNames[Area]
+local desiredRod = ""
+if Area == "Fishing" and not checkForRod("Wooden Fishing Rod") then
+    Network.Instancing_FireCustomFromClient:FireServer("Fishing", "ClaimRod") 
+    desiredRod = "Pro Fishing Rod"
+elseif Area == "AdvancedFishing" and not checkForRod("Amethyst Fishing Rod") then
+    desiredRod = "Amethyst Fishing Rod"
+end
+
+while true do task.wait(3)
+    if areaRodNames and not checkForRod(desiredRod) then
+        for rodName, price in pairs(areaRodNames) do
+            if not checkForRod(rodName) and Library.CurrencyCmds.Get("Fishing") > price[1] then
+                buyRod(rodName)
+                warn("bought "..rodName, price[1])
+            end
+        end
+    end
+end
+    task.wait(loopDelay)
+end
