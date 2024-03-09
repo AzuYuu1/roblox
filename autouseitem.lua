9cef4115bb065afc4a603bf4c9a194b3a39e810e
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-09 11:29:04 +0700

    Update autouseitem.lua

--- autouseitem.lua
+++ autouseitem.lua
@@ -31,33 +31,31 @@ game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Squea
 task.wait(3.5)
 --game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("69c7fbeed3524224aaabeb54e3b2e1ee",2)
 --game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("9fb26a7a409f4965b18d88b3d78651d2",2)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("b854636485bc46c69e07108ed2c99194",5)
+--fluxus
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("b854636485bc46c69e07108ed2c99194",20)
+task.wait(0.5)
+--codex
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Fruits: Consume"):FireServer("49244dda524848b19d42438af7b337ff",20)
 
 task.wait(3.5)
+--fluxus
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("6d8605ac7bda42508ba13fcf9893dbf7")
 task.wait(0.5)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("36b2906ccac548a48536f3fdca803bdc")
 task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("6d8605ac7bda42508ba13fcf9893dbf7")
+--codex
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("d35c953fa68a441da7b40f560a00bedb",5)
 task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("36b2906ccac548a48536f3fdca803bdc")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("993d741352e8418d8dd58b1da6d3198c",5)
 task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("6d8605ac7bda42508ba13fcf9893dbf7")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("d35c953fa68a441da7b40f560a00bedb",5)
 task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("36b2906ccac548a48536f3fdca803bdc")
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("6d8605ac7bda42508ba13fcf9893dbf7")
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("36b2906ccac548a48536f3fdca803bdc")
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("6d8605ac7bda42508ba13fcf9893dbf7")
-task.wait(0.5)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("36b2906ccac548a48536f3fdca803bdc")
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Potions: Consume"):FireServer("993d741352e8418d8dd58b1da6d3198c",5)
+
+
 
 task.wait(5)
 --mang sach coin 7
-task.wait(0.3)
-game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
 task.wait(0.3)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Enchants_Equip"):FireServer("7a2dd835a20340a89a46ed200e6aa2e2")
 task.wait(0.3)
