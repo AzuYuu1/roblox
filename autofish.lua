22563f76735cae2c6ff2b31bc7bcb72ddce4f267
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-09 19:47:53 +0700

    Update autofish.lua

--- autofish.lua
+++ autofish.lua
@@ -1,7 +1,7 @@
 
 
 --teleport
-repeat task.wait() until game:IsLoaded() task.wait(1)
+repeat task.wait() until game:IsLoaded() task.wait(2.5)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.workspace.__THINGS.Instances.Fishing.Teleports.Enter.CFrame.Position)
 task.wait(15)
 game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer("Fishing", "ClaimRod",Vector3.new(1139, 75, -3445))
