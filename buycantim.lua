6f7b485e8d7188f47648545e0136b2b3d4c12630
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-10 20:32:55 +0700

    Update buycantim.lua

--- buycantim.lua
+++ buycantim.lua
@@ -1,5 +1,6 @@
 _G.loop = true
 _G.loopDelay = 5
+while _G.loop do
 local saveModule = require(game:GetService("ReplicatedStorage").Library.Client.Save).Get()
 local Network = game.ReplicatedStorage:WaitForChild('Network')
 local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
@@ -24,7 +25,6 @@ function checkForRod(rodName)
     end
     return false
 end
-while _G.loop do
 function buyRod(rodName) return Network:WaitForChild("FishingMerchant_PurchaseRod"):InvokeServer(rodName) end
 
 local Area = tostring(getActive())
