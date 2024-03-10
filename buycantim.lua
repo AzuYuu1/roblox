2cc9bac2484ef2a14af37641ac4f988aca451098
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-10 12:09:45 +0700

    Update buycantim.lua

--- buycantim.lua
+++ buycantim.lua
@@ -1,5 +1,5 @@
 _G.loop = true
-_G.loopDelay = 0.00001
+_G.loopDelay = 20
 while _G.loop do
 local saveModule = require(game:GetService("ReplicatedStorage").Library.Client.Save).Get()
 local Network = game.ReplicatedStorage:WaitForChild('Network')
