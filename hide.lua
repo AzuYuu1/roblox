42093c18113dfd0269cb010e1eb0307bef52a27c
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-11 14:41:33 +0700

    Update hide.lua

--- hide.lua
+++ hide.lua
@@ -1,1 +1,41 @@
 
+repeat
+    task.wait()
+until game:IsLoaded()
+
+local Workspace = game:GetService("Workspace")
+local Terrain = Workspace:WaitForChild("Terrain")
+Terrain.WaterReflectance = 0
+Terrain.WaterTransparency = 1
+Terrain.WaterWaveSize = 0
+Terrain.WaterWaveSpeed = 0
+
+local Lighting = game:GetService("Lighting")
+Lighting.Brightness = 0
+Lighting.GlobalShadows = false
+Lighting.FogEnd = 9e100
+Lighting.FogStart = 0
+
+sethiddenproperty(Lighting, "Technology", 2)
+sethiddenproperty(Terrain, "Decoration", false)
+
+local Model = workspace
+local ClassTypes = {
+	"Decal",
+        "Graph",
+	"Texture",
+        "TextLabel",
+        "Terrain",
+        "Part",
+        "Mesh",
+        "MeshPart",
+
+}
+
+for _, v in ipairs(workspace:GetDescendants()) do
+	if table.find(ClassTypes, v.ClassName) then
+		print(v.ClassName)
+		--test the script before deleting, remove the dashes beside the line below when ur ready
+		v:Destroy()
+	end
+end
