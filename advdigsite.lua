d89d090ba35f5bf57f2b8d5382e91924d44fa0d4
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-11 19:58:24 +0700

    Update and rename autolootshovel.lua to advdigsite.lua

---
+++ advdigsite.lua
@@ -0,0 +1,70 @@
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+--auto claim mail
+task.spawn(function()
+    while task.wait() do
+        game:GetService("ReplicatedStorage").Network["Mailbox: Claim All"]:InvokeServer()
+        task.wait(5)
+    end
+end)
+
+tasl.wait(1)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.workspace.__THINGS.Instances.Digsite.Teleports.Enter.CFrame.Position)
+task.wait(5)
+game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer("Digsite", "ClaimShovel")
+task.wait(35)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.workspace.__THINGS.Instances.Digsite.Teleports.Leave.CFrame.Position)
+
+
+task.spawn(function()
+    while task.wait() do
+local saveModule = require(game:GetService("ReplicatedStorage").Library.Client.Save).Get()
+local Network = game.ReplicatedStorage:WaitForChild('Network')
+local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
+
+local shovelNames = {
+
+    AdvancedDigsite = {
+        ["Amethyst Shovel"] = {12500000},
+    },
+}
+
+function getActive() return workspace.__THINGS.__INSTANCE_CONTAINER.Active:GetChildren()[1] end
+
+function checkForShovel(shovelName)
+    for uid,value in pairs(saveModule.Inventory.Misc)  do
+        for _,v in pairs(value) do
+            if v == shovelName  then
+                return true 
+            end
+        end
+    end
+    return false
+end
+
+function buyShovel(shovelName) return Network:WaitForChild("DigsiteMerchant_PurchaseShovel"):InvokeServer(shovelName) end
+
+local Area = tostring(getActive())
+local areaShovelNames = shovelNames[Area]
+local desiredShovel = ""
+if Area == "Digsite" and not checkForShovel("Wooden Shovel") then
+    Network.Instancing_FireCustomFromClient:FireServer("Digsite", "ClaimShovel") 
+    desiredShovel = "Pro Shovel"
+elseif Area == "AdvancedDigsite" and not checkForShovel("Amethyst Shovel") then
+    desiredShovel = "Amethyst Shovel"
+end
+
+while true do task.wait(3)
+    if areaShovelNames and not checkForShovel(desiredShovel) then
+        for shovelName, price in pairs(areaShovelNames) do
+            if not checkForShovel(shovelName) and Library.CurrencyCmds.Get("Digsite") > price[1] then
+                buyShovel(shovelName)
+                warn("bought "..shovelName, price[1])
+            end
+        end
+    end
+end
+        task.wait(5)
+    end
+end)
+
