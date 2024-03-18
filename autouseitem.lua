7b42d0646516df4321b737075ed4d750e382d482
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-18 08:45:39 +0700

    Update autouseitem.lua

--- autouseitem.lua
+++ autouseitem.lua
@@ -8,6 +8,51 @@ end)
 
 task.spawn(function()
     while task.wait(1.5) do
+local fruitsModule = require(game:GetService("ReplicatedStorage").Library.Client.FruitCmds)
+
+local fruitIds = {}
+local neededFruits = {}
+
+for itemId, itemData in pairs(require(game:GetService("ReplicatedStorage").Library).Save.Get().Inventory.Fruit) do
+    if itemData["id"] ~= "Candycane" then
+        local fruitName = itemData["id"]
+        fruitIds[fruitName] = itemId
+        table.insert(neededFruits, fruitName)
+    end
+end
+
+for fruitName, fruitData in pairs(fruitsModule.GetActiveFruits()) do
+    if #fruitData < 20 then
+        fruitsModule.Consume(fruitIds[fruitName], (20 - #fruitData))
+    end
+    for i = 1, #neededFruits do
+        if neededFruits[i] == fruitName then
+            table.remove(neededFruits, i)
+        end
+    end
+    task.wait(0.15)
+end
+
+for _, fruit in neededFruits do
+    fruitsModule.Consume(fruitIds[fruit], 20)
+    task.wait(0.15)
+end
+
+require(game:GetService("ReplicatedStorage").Library.Client.Network).Fired("Fruits: Update"):Connect(function(data)
+    task.wait(1)
+    for fruitName, fruitData in pairs(data) do
+        if #fruitData < 20 then
+            fruitsModule.Consume(fruitIds[fruitName])
+            task.wait(0.15)
+        end
+    end
+end)
+task.wait(2)
+    end
+end)
+
+task.spawn(function()
+    while task.wait(1.5) do
 getgenv().autoMisc = {
     autoEnchant = true,
     enchantConfig = {
