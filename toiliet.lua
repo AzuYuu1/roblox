2da2192e30b1d939198bda5e428f6f14f2408864
AzuYuu1 <121219497+AzuYuu1@users.noreply.github.com>
2024-03-23 22:25:46 +0700

    Create toiliet.lua

---
+++ toiliet.lua
@@ -0,0 +1,4 @@
+print("Made By SoraYuu | https://x.com/SoraYuuEu)")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AzuYuu1/roblox/main/fpsboot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AzuYuu1/roblox/main/ttd.lua"))()
+
